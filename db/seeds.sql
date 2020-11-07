c733ba0a41422d0a4723e362e68a0656b5cdd163
MichaelMayor <michael.davidge.mayor@gmail.com>
2020-11-07 18:28:48 -0500

    Created burgers db

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+use burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Delicious Burger");
+INSERT INTO burgers (burger_name) VALUES ("Tasty Burger");
+INSERT INTO burgers (burger_name) VALUES ("Okay Burger");
